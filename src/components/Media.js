1a8b596c5a2f7969a494b01fe6f1aa4fd86aa270
John <jonathancbatista@gmail.com>
2020-05-27 17:26:53 -0300

    Add links and right button to media obj

--- src/components/Media.js
+++ src/components/Media.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from "react-router-dom"
 
 class Media extends Component {
   render() {
@@ -7,23 +8,29 @@ class Media extends Component {
 
     return (
         <div className="media">
-          <figure className="media-left">
-            <p className="image is-64x64">
-              <img src={mediaData.mediaImg} alt={mediaData.mediaAlt} />
-            </p>
-          </figure>
+          <Link to={mediaData.mediaLink}>
+            <figure className="media-left">
+              <p className="image is-64x64">
+                <img src={mediaData.mediaImg} alt={mediaData.mediaAlt} />
+              </p>
+            </figure>
+          </Link>
 
           <div className="media-content">
             <div className="content">
               <p>
-                <strong>{mediaData.mediaTitle}</strong> 
+                <Link to={mediaData.mediaLink}><strong>{mediaData.mediaTitle}</strong></Link>
                 <br/><small>{mediaData.mediaSubtitle}</small>
                 <br />
                 {mediaData.mediaContent}
               </p>
             </div>
           </div>
-
+          <div className="media-right">
+            <Link to={mediaData.mediaLink}>
+              <button className="button is-small is-warning">Veja mais</button>
+            </Link>
+          </div>
         </div>
     )
   }
