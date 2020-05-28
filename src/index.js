c261f5aba2c44bd44abad876ce3d3924790d6e7d
John <jonathancbatista@gmail.com>
2020-05-28 16:17:49 -0300

    enable serviceworker

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-
-/* import BrowserRouter from 'react-router-dom' */
 import { BrowserRouter } from "react-router-dom";
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
 	<BrowserRouter basename={process.env.PUBLIC_URL} >
 		<App />
 	</BrowserRouter>,
 	 document.getElementById('root'))
+
+serviceWorker.register();
