cf299818c70e0b09bb1b9ef32a2e6d2611a6820d
John <jonathancbatista@gmail.com>
2020-05-28 14:41:39 -0300

    Updates

---
+++ precache-manifest.daddac38d899ad761cc9a1101780e43b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ea9890deb5d2ac8e6dbb83cbac04d28",
+    "url": "/maxtracktest/index.html"
+  },
+  {
+    "revision": "7253daf93644c4ec369a",
+    "url": "/maxtracktest/static/css/main.9f8bdd2b.chunk.css"
+  },
+  {
+    "revision": "e6467e48c99c1aa780e3",
+    "url": "/maxtracktest/static/js/2.281c12aa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/maxtracktest/static/js/2.281c12aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7253daf93644c4ec369a",
+    "url": "/maxtracktest/static/js/main.fe72ba05.chunk.js"
+  },
+  {
+    "revision": "74899517b0bbc255b971",
+    "url": "/maxtracktest/static/js/runtime-main.ec5379eb.js"
+  },
+  {
+    "revision": "86ff7c17f7e92af5da77274697bbff25",
+    "url": "/maxtracktest/static/media/logo.86ff7c17.svg"
+  }
+]);
